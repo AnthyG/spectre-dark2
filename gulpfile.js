60903f53617ee4defa812e75789cb2fc1b5228dd
Anthony Garratt <anthygarratt@aol.de>
2017-11-22 21:29:41 +0100

    Revert "First changes"
    This reverts commit 7f4eb762d09a151ee2bcf7b55bdce399e343c2f2.

--- gulpfile.js
+++ gulpfile.js
@@ -6,51 +6,50 @@ var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
-    source: './src/*.scss',
-    doc: './docs/src/*.scss'
+  source: './src/*.scss',
+  doc: './docs/src/*.scss'
 };
 
 gulp.task('watch', function() {
-    gulp.watch('./**/*.scss', ['build']);
-    gulp.watch('./**/*.scss', ['docs']);
+  gulp.watch('./**/*.scss', ['build']);
+  gulp.watch('./**/*.scss', ['docs']);
 });
 
 gulp.task('build', function() {
-    gulp.src(paths.source)
-        .pipe(sass({ outputStyle: 'compact', precision: 10 })
-            .on('error', sass.logError)
-        )
-        .pipe(autoprefixer())
-        .pipe(csscomb())
-        .pipe(gulp.dest('./dist'))
-        .pipe(cleancss())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./dist'))
-        .pipe(gulp.dest('D:/ZeroNet-win-dist/data/1CWkZv7fQAKxTVjZVrLZ8VHcrN6YGGcdky/css'));
+  gulp.src(paths.source)
+    .pipe(sass({outputStyle: 'compact', precision: 10})
+      .on('error', sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(csscomb())
+    .pipe(gulp.dest('./dist'))
+    .pipe(cleancss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('docs', function() {
-    gulp.src(paths.doc)
-        .pipe(sass({ outputStyle: 'compact', precision: 10 })
-            .on('error', sass.logError)
-        )
-        .pipe(autoprefixer())
-        .pipe(csscomb())
-        .pipe(gulp.dest('./docs/css'));
-    gulp.src(paths.source)
-        .pipe(sass({ outputStyle: 'compact', precision: 10 })
-            .on('error', sass.logError)
-        )
-        .pipe(autoprefixer())
-        .pipe(csscomb())
-        .pipe(gulp.dest('./docs/dist'))
-        .pipe(cleancss())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./docs/dist'));
+  gulp.src(paths.doc)
+    .pipe(sass({outputStyle: 'compact', precision: 10})
+      .on('error', sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(csscomb())
+    .pipe(gulp.dest('./docs/css'));
+  gulp.src(paths.source)
+    .pipe(sass({outputStyle: 'compact', precision: 10})
+      .on('error', sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(csscomb())
+    .pipe(gulp.dest('./docs/dist'))
+    .pipe(cleancss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./docs/dist'));
 });
 
 gulp.task('default', ['build']);
